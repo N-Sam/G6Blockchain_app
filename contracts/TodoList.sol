3071e7bf138410ece409f143839739a85bc63692
N-Sam <nyekosamueltreg@gmail.com>
2021-07-23 00:23:30 +0300

    Feat: added constructor to taskfunction

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,8 +1,24 @@
+/// SPDL-License identifier: AFL-3.0
+
 pragma solidity ^0.5.0;
 
-contract TodoList{
-    
-    uint public taskCount = 0;
+contract TodoList {
+    uint256 public taskCount = 0;
 
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
 
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createtask("Check out aws assignment");
+    }
+
+    function createtask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
